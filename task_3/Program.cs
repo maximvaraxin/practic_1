2a69e38a616a29c58273d90dcf5611881cf9a30b
Максим Вараксин <maks-02@yandex.ru>
2022-06-23 21:53:10 +0300

    add task_3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*7. Напишите программу, которая принимает на вход 
+ * трёхзначное число и на выходе показывает последнюю
+ * цифру этого числа.
+ * 456 -> 6
+ * 782 -> 2
+ * 918 -> 8
+*/
+
+Console.WriteLine($"Введите число");
+string num = Console.ReadLine();
+
+if (num.Length == 3){ Console.WriteLine( $"Последняя цифра: {num[num.Length - 1]}"); }
+if (num.Length < 3 || num.Length - 1 > 3){ Console.WriteLine($"Введено не 3-х значное число!"); }
+
