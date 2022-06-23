f336790540c5307f3560ca0a12f1a8aa69fb47c7
Максим Вараксин <maks-02@yandex.ru>
2022-06-23 21:25:58 +0300

    update task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -3,3 +3,13 @@
  * (N), а на выходе показывает все целые числа в 
  * промежутке от -N до N.
  */
+
+Console.WriteLine($"Введите размер диапазона");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+
+while(count <= num)
+{
+    Console.WriteLine(count);
+    count++;
+}
