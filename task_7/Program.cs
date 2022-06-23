c7ab9eb652a5ede2e55d2c2116078144b2acc8f9
Максим Вараксин <maks-02@yandex.ru>
2022-06-24 00:39:56 +0300

    add task_7

---
+++ task_7/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+ * 5 -> 2, 4
+ * 8 -> 2, 4, 6, 8
+ */
+
+Console.WriteLine($"Введите N");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if( num > 0)
+{
+    while(count <= num)
+    {
+        if(count % 2 == 0){ Console.WriteLine($"Найдено четное ччисло в диапазоне от 1 до {num}: {count}"); }
+        count++;
+    }
+}
+else
+{
+    Console.WriteLine($"Введите число больше 0!");
+}
